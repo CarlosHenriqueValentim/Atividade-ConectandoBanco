9838b1663652b47b87c677e36e62a0719ae3a92d
Carlos Henrique Valentim Silva <rikcar22@gmail.com>
2025-11-05 15:43:36 -0300

    Up

--- MySQL ( Script )/ConectandoBanco.sql
+++ MySQL ( Script )/ConectandoBanco.sql
@@ -3,11 +3,12 @@ use escola;
 
 create table if not exists alunos (
   Id int auto_increment primary key,
-  Nome VARCHAR(100) NOT NULL,
-  Idade INT NOT NULL,
-  Curso VARCHAR(50) NOT NULL
+  Nome VARCHAR(100) not null,
+  Idade INT not null,
+  Curso VARCHAR(50) not null
 );
 
+# Testes
 select * from alunos;
 delete from alunos;
 truncate table alunos;
