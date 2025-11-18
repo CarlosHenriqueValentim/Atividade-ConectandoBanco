cae26ca3fb549848e602eff800bf77111b9329aa
CarlosHenriqueValentim <rikcar22@gmail.com>
2025-11-18 09:32:16 -0300

    up

--- MySQL ( Script )/Escola.sql
+++ MySQL ( Script )/Escola.sql
@@ -1,11 +1,10 @@
 create database if not exists escola;
 use escola;
-
 create table if not exists alunos (
-  Id int auto_increment primary key,
-  Nome varchar(100) not null, # Na apostila Nome esta tipo INT (Númemros inteiros) Corrigi utilizando Varchar para armazenar letras e não Números. 
-  Idade int not null,
-  Curso varchar(50) not null
+Id int auto_increment primary key,
+Nome varchar(100) not null, # Na apostila Nome esta tipo INT (Númemros inteiros) Corrigi utilizando Varchar para armazenar letras e não Números. 
+Idade int not null,
+Curso varchar(50) not null
 );
 
 # Testes
